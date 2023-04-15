fec26af17abf272e4e9ab87c917b700051d76b6b
han sheng <gh890528@gmail.com>
2023-04-15 23:49:22 +0800

    基本拖拉未更新資料庫

---
+++ app/javascript/controllers/sortable_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+import Sortable from 'sortablejs'
+// Connects to data-controller="sortable"
+export default class extends Controller {
+  connect() {
+    Sortable.create(this.element, {})
+  }
+}
