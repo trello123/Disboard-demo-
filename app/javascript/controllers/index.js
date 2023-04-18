de083ab3110d3a8f849b5748fe9bae57f5be7323
han sheng <gh890528@gmail.com>
2023-04-18 16:49:05 +0800

    fixed conflict

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,5 +10,9 @@ application.register("aos", AosController)
 import CardFormController from "./card_form_controller"
 application.register("card-form", CardFormController)
 
+
+import AosController from "./aos_controller"
+application.register("aos", AosController)
+
 import SortableController from "./sortable_controller"
 application.register("sortable", SortableController)
