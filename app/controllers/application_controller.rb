ea0df75e9c96a8cc691158db43d8a11bf48423bc
sdaru <sdaru18@gmail.com>
2023-04-19 13:47:24 +0800

    Change Path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  def after_sign_in_path_for(resource)
+    cards_path
+  end
 
   private
   def record_not_found
