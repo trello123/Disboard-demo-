7865e9f00fb62990c9be94f5995c6fcc832818ab
Eric <eric00471@gmail.com>
2023-04-21 01:20:08 +0800

    finish homepage RWD

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ gem "requestjs-rails", "~> 0.0.10"
 gem "acts_as_list", "~> 1.1"
 
 gem "rspec-rails", "~> 6.0"
+
+gem "warden", "~> 1.2"
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+          'homepage-bg': '#414E92'
+      },
     },
   },
   plugins: [
