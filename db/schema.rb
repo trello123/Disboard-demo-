908d6c584e709e3b5ce6742e8cdf336cb286002d
Eric <eric00471@gmail.com>
2023-04-25 21:41:53 +0800

    rebase bug fixed

--- db/schema.rb
+++ db/schema.rb
@@ -38,13 +38,11 @@ ActiveRecord::Schema.define(version: 2023_04_25_103006) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "position"
     t.bigint "container_id"
-    t.string "slug"
     t.datetime "daybegin"
     t.datetime "deadline"
     t.datetime "deleted_at"
     t.index ["container_id"], name: "index_cards_on_container_id"
     t.index ["deleted_at"], name: "index_cards_on_deleted_at"
-    t.index ["slug"], name: "index_cards_on_slug", unique: true
   end
 
   create_table "containers", force: :cascade do |t|
@@ -53,17 +51,6 @@ ActiveRecord::Schema.define(version: 2023_04_25_103006) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["board_id"], name: "index_containers_on_board_id"
-  end
-
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   create_table "users", force: :cascade do |t|
