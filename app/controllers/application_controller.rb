0ba14cd02b5404614ab833aa3a26f8899b46b163
han sheng <gh890528@gmail.com>
2023-04-26 14:36:10 +0800

    聊天室完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  helper_method :current_user
+
+  def current_user
+    current_user ||= super
+  end
   def after_sign_in_path_for(resource)
     boards_path
   end
