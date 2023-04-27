d34d04c6d0342a8c2ce4a258901096e022430f6f
EazyChen <eazychen@EazyChendeMacBook-Air.local>
2023-04-27 20:41:32 +0800

    pull dev

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,44 @@
+import consumer from "./consumer"
+
+
+document.addEventListener('turbolinks:load', ()=> {
+  const room_element = document.getElementById('room-id')
+  const room_id = room_element.getAttribute('data-room-id')
+
+  // window.subscriptions = consumer.subscriptions
+  // console.log(JSON.parse(subscriptions.subscriptions[0].identifier).channel);
+
+  consumer.subscriptions.subscriptions.forEach((subscription) => {
+    // if(JSON.parse(subscription.identifier).channel == 'RoomChannel')
+    consumer.subscriptions.remove(subscription)
+  })
+
+  consumer.subscriptions.create({channel:"RoomChannel", room_id: room_id},{
+    connected() {
+      console.log("connected to room channel" + room_id);
+      // Called when the subscription is ready for use on the server
+    },
+  
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+  
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      console.log(data);
+      const user_element = document.getElementById('user-id')
+      const user_id = Number(user_element.getAttribute('data-user-id'))
+      
+      let html
+      
+      if(user_id === data.message.user_id){
+        html = data.mine 
+      }else{
+        html = data.theirs
+      }
+      
+      const messageContainer = document.getElementById('messages')
+      messageContainer.innerHTML = messageContainer.innerHTML + html
+    }
+  });
+})
