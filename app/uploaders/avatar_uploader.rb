fb330a2571c46e172f2f2c4561e5eff34e130a78
EazyChen <eazychen@EazyChendeMacBook-Air.local>
2023-04-28 11:38:01 +0800

    deleted picture

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -26,21 +26,4 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
-  #Process files as they are uploaded:
-  #process scale: [100, 100]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
- # Create different versions of your uploaded files:
- # version :thumb do
- # process resize_to_fit: [50, 50]
- # end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 end
