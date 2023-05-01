6711c5fad9fe724cd439e66c7dda1766dc0bec58
han sheng <gh890528@gmail.com>
2023-05-01 17:22:16 +0800

    new message alert

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,19 +1,19 @@
 import consumer from "./consumer"
+import Swal from "sweetalert2"
 
 
 document.addEventListener('turbolinks:load', ()=> {
   const room_element = document.getElementById('room-id')
   const room_id = room_element.getAttribute('data-room-id')
-
-
+  window.subscriptions = consumer.subscriptions
   consumer.subscriptions.subscriptions.forEach((subscription) => {
-    // if(JSON.parse(subscription.identifier).channel == 'RoomChannel')
-    consumer.subscriptions.remove(subscription)
+    if (JSON.parse(subscription.identifier).channel == 'RoomChannel')
+      consumer.subscriptions.remove(subscription)
   })
 
   consumer.subscriptions.create({channel:"RoomChannel", room_id: room_id},{
     connected() {
-      console.log("connected to room channel" + room_id);
+      console.log("connected to room channel" + room_id)
       // Called when the subscription is ready for use on the server
     },
   
@@ -23,7 +23,6 @@ document.addEventListener('turbolinks:load', ()=> {
   
     received(data) {
       // Called when there's incoming data on the websocket for this channel
-      console.log(data);
       const user_element = document.getElementById('user-id')
       const user_id = Number(user_element.getAttribute('data-user-id'))
       
@@ -33,6 +32,7 @@ document.addEventListener('turbolinks:load', ()=> {
         html = data.mine 
       }else{
         html = data.theirs
+        successNotify()
       }
       
       const messageContainer = document.getElementById('messages')
@@ -41,5 +41,27 @@ document.addEventListener('turbolinks:load', ()=> {
       const chatWindow = document.getElementById('chat-window')
       chatWindow.scrollTop = chatWindow.scrollHeight
     }
-  });
+  })
 })
+
+function successNotify(position = "top-end", timer = 2000) {
+  const Toast = Swal.mixin({
+    toast: true,
+    position,
+    showConfirmButton: false,
+    timer,
+    timerProgressBar: true,
+    showCloseButton: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer)
+      toast.addEventListener("mouseleave", Swal.resumeTimer)
+    },
+  })
+
+  Toast.fire({
+    title: '您有新訊息',
+  })
+}
+
+
+
