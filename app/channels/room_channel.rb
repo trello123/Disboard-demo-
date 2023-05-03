974848abb24fc94b0cfbde8b2ac1488c88bbb12b
han sheng <gh890528@gmail.com>
2023-05-03 14:42:24 +0800

    Fix chat room n+1 question

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -7,9 +7,4 @@ class RoomChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
-
-  def receive(data)
-    # 在接收到新訊息時，透過 broadcast_to 方法將訊息發送給其他客戶端
-    ActionCable.server.broadcast "room_channel_#{params['room_id']}", {message: data['message']}
-  end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
 
   def index
-    @messages = Message.all
+    @messages = Message.includes(:user)
   end
   def new
     @message = Message.new
