760b28d6ba77c53b4eb3cd0cb58f2dd3645c133b
Eric <eric00471@gmail.com>
2023-05-07 03:27:51 +0800

    modified charts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,7 @@ module ApplicationHelper
   end 
 
   def assign_user(board)
-    board.users.pluck(:email, :id).unshift(["尚未指派","尚未指派"])
+    board.users.pluck(:email, :email).unshift(["尚未指派","尚未指派"])
   end
 
   def find_user_email(card)
--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -4,9 +4,18 @@ module BoardsHelper
   end 
 
   def group_cards_status(container, column)
-    container.joins(:cards).group(:position, column).count
+    container.joins(:cards).group(:position, column).count.transform_keys { |k| "#{k[1]}" }
   end
 
+  def calculate_done_cards(card, container, column)
+    if group_cards_status(container, column)["已完成"].present?
+      "#{(group_cards_status(container, column)["已完成"] / (card.count * 1.0) * 100).round}#{"%"}"
+    else
+      "-"
+    end
+  end
+
+  ##暫時留著，之後可能要用到
   def card_assigned_status(user, column)
     user.joins(:boards).includes(:cards).group(:username, column).count
   end
