27b1b896b252f8130129eab1a37d3838d0ad7cc4
han sheng <gh890528@gmail.com>
2023-05-06 21:33:09 +0800

    fixed page style

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -21,13 +21,14 @@ module.exports = {
           'Azure-blue-5': '#6871B1',
           'Azure-blue-4': '#5965A3',
           'Azure-blue-3': '#7A84C9',
-          'Azure-blue-2': '#515D8E',
+          'Azure-blue-2': '#2C3E50',
           'Azure-blue-1': '#6E78A7',
           'my-blue': '#2196f3',
-          'white-1':'#f5f5f5'
+          'white-1':'#f5f5f5',
+          'white-2':'#CCCCCC',
       },
       boxShadow: {
-        '3xl': '30px 0x 0px 0px rgba(0, 0, 0, 0.3)',
+        '3xl': 'rgba(67, 71, 85, 0.27) 0px 0px 0.25em, rgba(90, 125, 188, 0.05) 0px 0.25em 1em;',
         '4xl': 'rgb(38, 57, 77) -20px 20px 30px 0px;',
         '5xl': 'rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;'
       }
