e1f13cf06d0eba19d670f57d93253e28e152d830
Eric <eric00471@gmail.com>
2023-05-08 19:57:23 +0800

    bug fixed

--- app/javascript/utils/notify.js
+++ app/javascript/utils/notify.js
@@ -2,7 +2,7 @@ import Swal from "sweetalert2"
 import moment from "moment"
 import { patch } from '@rails/request.js'
 
-function successNotify(message, position = "top", timer = 2000) {
+function successNotify(message, icon = "success", position = "top", timer = 2000) {
   const Toast = Swal.mixin({
     toast: true,
     position,
@@ -17,7 +17,7 @@ function successNotify(message, position = "top", timer = 2000) {
   })
 
   Toast.fire({
-    icon: "success",
+    icon,
     title: message,
   })
 }
--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -2,12 +2,20 @@
 #
 # Table name: cards
 #
-#  id         :bigint           not null, primary key
-#  title      :string
-#  intro      :text
-#  level      :integer          default(0)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :bigint           not null, primary key
+#  title        :string
+#  intro        :text
+#  level        :integer          default("待確認")
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  position     :integer
+#  container_id :bigint
+#  daybegin     :datetime
+#  deadline     :datetime
+#  deleted_at   :datetime
+#  avatar       :string
+#  board_id     :bigint
+#  assigned_to  :string           default("尚未指派")
 #
 require "test_helper"
 
