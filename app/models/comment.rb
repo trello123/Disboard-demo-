9f73c6210bf4dab3f29874d08dec428a0a305693
sdaru <sdaru18@gmail.com>
2023-05-10 18:08:31 +0800

    補上comment 文字編輯器

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,12 @@
 class Comment < ApplicationRecord
+  # relationships
   belongs_to :user
   belongs_to :card
 
   #套件給的
   acts_as_paranoid
+
+  # 文字編輯器
+  has_rich_text :intro
+  has_one_attached :avatar
 end
