a1278d343532cd848114499defa58b29d7f998ff
han sheng <gh890528@gmail.com>
2023-05-11 20:07:08 +0800

    fixed  animation

--- app/javascript/controllers/intro_controller.js
+++ app/javascript/controllers/intro_controller.js
@@ -13,33 +13,40 @@ export default class extends Controller {
       exitOnOverlayClick: false,
       showStepNumbers: true,
       disableInteraction: true,
+      highlightClass: 'custom-highlight',
+      tooltipClass: 'usertheach',
       nextLabel: '下一步',
       prevLabel: '上一步',
-      skip: '',
       doneLabel: '開始使用吧～',
       steps: [
         {
-          intro: '歡迎加入disboard'
+          title: '歡迎加入disboard',
+          intro: '使用教學'
         },
         {
+          title: '步驟一:新增專案群組',
           intro: '點擊這裡可以新增專案群組',
           element: '.addboard',
         },
         {
+          title: '步驟二:群組功能總覽',
           intro: '點擊這裡可以看到群組裡面的功能總覽',
           element: '.boardlist'
         },
         {
+          title: '步驟三:新增專案任務',
           intro: '點擊這裡可以新增任務',
           element: '.addcard',
         },
         {
+          title: '步驟四:個人所有專案群組',
           intro: '點擊這裡可以看到你的所有專案群組',
           element: '.user',
         },
         {
+          title: '步驟五:快捷鍵教學',
           intro: 
-          '最後還有網站的快捷鍵：</br>alt+s可以新增狀態</br>alt+c可以新增卡片</br>alt+d可以切換到日曆頁面</br>alt+m可以切換到聊天室</br>alt+w可以切換到任務列表</br>alt+f可以切換到成員</br>alt+q可以切換到儀表',
+          '最後還有網站的快捷鍵：</br>alt/option+s可以新增狀態</br>alt/option+c可以新增卡片</br>alt/option+d可以切換到日曆頁面</br>alt/option+m可以切換到聊天室</br>alt/option+w可以切換到任務列表</br>alt/option+f可以切換到成員</br>alt/option+q可以切換到儀表',
         },
       ],
     });
