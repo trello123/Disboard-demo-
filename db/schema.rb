da88e06a1739c05c969cdca63a5f5995cb042974
Eric <eric00471@gmail.com>
2023-05-15 21:07:55 +0800

    add tooltip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_07_124134) do
+ActiveRecord::Schema.define(version: 2023_05_13_184025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
