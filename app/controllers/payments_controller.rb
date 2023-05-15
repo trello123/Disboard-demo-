f527ecfda2708454e7d973729a1d547445ac2018
Eric <eric00471@gmail.com>
2023-05-15 12:00:09 +0800

    modified styles

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,22 +4,27 @@ class PaymentsController < ApplicationController
 
   def index
     @form_info = Newebpay::Mpg.new(30, current_user.id).form_info
-    @form_info2 = Newebpay::Mpg.new(60, current_user.id).form_info
+    @form_info2 = Newebpay::Mpg.new(49, current_user.id).form_info
   end
 
   def create
     # 要記得附上該筆訂單的資訊，才有辦法建立付款喔！
     @form_info = Newebpay::Mpg.new(30, current_user.id).form_info
-    @form_info2 = Newebpay::Mpg.new(60, current_user.id).form_info
+    @form_info2 = Newebpay::Mpg.new(49, current_user.id).form_info
   end
 
   def notify_response
     response = Newebpay::MpgResponse.new(params[:TradeInfo])
     @vip_id = response.order_no.split("_").last
+    @amt = response.amt
 
     if response.success?
-      User.find(@vip_id).update(status: "Vip")
-      redirect_to root_path
+      if @amt == 30
+        User.find(@vip_id).update(status: "Standard")
+      elsif @amt == 49
+        User.find(@vip_id).update(status: "Vip")
+      end
+      redirect_to payments_path
     else
       redirect_to boards_path
     end
