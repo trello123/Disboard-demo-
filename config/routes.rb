d03846ee7cc5033ca0c49e0024cbb45b0e08cf43
han sheng <gh890528@gmail.com>
2023-05-22 15:00:16 +0800

    add user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: 'users/registrations' }
   # resources :cards
   
   resources :boards do
