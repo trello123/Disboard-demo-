4fcfc7244992fdafb3cd134f7974af4172df0c53
sdaru <sdaru18@gmail.com>
2023-05-23 14:43:44 +0800

    修掉個人資訊需要密碼認證

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
       find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
         user.email = auth.info.email
         user.password = Devise.friendly_token[0, 20]
-        # user.name = auth.info.name   # assuming the user model has a name
+        user.username = auth.info.name   
         # user.image = auth.info.image # assuming the user model has an image
         # If you are using confirmable and the provider(s) you use validate emails, 
         # uncomment the line below to skip the confirmation emails.
