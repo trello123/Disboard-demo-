0a0dbb3bb6d4c885e8fec0e848f694dbf3ebf68a
sdaru <sdaru18@gmail.com>
2023-05-23 14:43:44 +0800

    修掉個人資訊需要密碼認證

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -52,10 +52,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
 
   def after_update_path_for(resource)
     boards_path
   end
+
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
